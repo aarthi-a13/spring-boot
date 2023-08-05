bac55697bf94c779dd0133d5f8a2edd955e0f637
Aarthi A <aarthialagarsamy13@gmail.com>
2023-08-05 20:17:49 +0530

    Spring boot hello world application

---
+++ content-calendar/src/main/java/com/aarthi/spring/contentcalendar/ContentCalendarApplication.java
@@ -0,0 +1,15 @@
+package com.aarthi.spring.contentcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentCalendarApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        SpringApplication.run(ContentCalendarApplication.class, args);
+    }
+
+
+}
