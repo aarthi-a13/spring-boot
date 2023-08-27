821f4ec673e40cf2d476f4608b6acf1a095cc0fb
Aarthi A <aarthiajith72@gmail.com>
2023-08-27 15:41:04 +0530

    Restructured spring boot crash project

--- CrashBasics/content-calendar/src/main/java/com/aarthi/contentcalendar/config/ContentCalendarProperties.java
+++
@@ -1,7 +0,0 @@
-package com.aarthi.contentcalendar.config;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(value = "cc")
-public record ContentCalendarProperties(String welcomeMsg, String about) {
-}
