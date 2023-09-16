9c7e131b8e14146ba250cdb6d10fdcfdc491cfff
Aarthi A <aarthiajith72@gmail.com>
2023-09-16 20:10:58 +0530

    Implemented monitoring and metrics

--- MicroServices-101/order-service/src/main/java/com/aarthi/ms/orderservice/OrderServiceApplication.java
+++ MicroServices-101/order-service/src/main/java/com/aarthi/ms/orderservice/OrderServiceApplication.java
@@ -1,12 +1,10 @@
 package com.aarthi.ms.orderservice;
 
-import org.apache.kafka.clients.admin.NewTopic;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
 @EnableDiscoveryClient
